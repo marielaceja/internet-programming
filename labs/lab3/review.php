5b5047638c5c743a7a4e00b1925f363b08f1d1ce
Mariela Ceja <maceja@csumb.edu>
2016-02-19 00:28:58 +0000

    added function learningArrays()

--- labs/lab3/review.php
+++ labs/lab3/review.php
@@ -1,21 +1,41 @@
 <?php
-//$prices = array(); //Initialize an empty array
-$prices = array(550,750,600);
 
-$prices[]=100; // add and element to the end of the array
-array_push($prices,200); //also adds element
-$prices[0]=500; // modifies first element
-
-print_r($prices);
-
-echo "<hr>";
-
-for($i=0;$i<count($prices);$i++){
-    //count returns the size of the array
-    echo "$$prices[$i]" . "<br />";
+function learningArrays(){
+    //$prices = array(); //Initialize an empty array
+    $prices = array(550,750,600);
+    
+    $prices[]=100; // add and element to the end of the array
+    array_push($prices,200); //also adds element
+    $prices[0]=500; // modifies first element
+    
+    print_r($prices);
+    
+    echo "<hr>";
+    
+    for($i=0;$i<count($prices);$i++){
+        //count returns the size of the array
+        //echo "$$prices[$i]" . "<br />";
+    }
+    
+    echo array_sum($prices);
+    
+    //removing elements from an array
+    unset($prices[2]);
+    print_r($prices);
+    echo "<hr />";
+    
+    sort($prices);
+    print_r($prices);
+    echo "<hr />";
+    
+    $prices = arra_values($prices);
+    echo "<hr />";
+    print_r($prices);
+    echo "<hr />";
 }
 
-echo array_sum($prices);
+
+
 
 ?>
 <!DOCTYPE html>
