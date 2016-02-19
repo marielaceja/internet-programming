56d2faf0a8b6ce2cbac39c69584e67724b009e46
Mariela Ceja <maceja@csumb.edu>
2016-02-19 00:57:46 +0000

    Added silverjack.php file and all images of cards to be played.

---
+++ labs/lab3/silverjack.php
@@ -0,0 +1,28 @@
+<?php
+
+$suit = array("clubs","diamonds","hearts","spades");
+echo "<img src=cards/clubs/1.png>";
+$randValue = rand(0,3);
+
+$suit[$randValue];
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Silver Jack Game</title>
+        <style>
+            h1{
+                text-align:center;
+                color:red;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Silver Jack Game</h1>
+    </body>
+</html>
