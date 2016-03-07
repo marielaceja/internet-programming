d9a9188739f97ca45e2b844557a4979f9f1f9dbc
Mariela Ceja <maceja@csumb.edu>
2016-03-07 02:32:02 +0000

    Finalized lab 4 with style implemented and validation on the form.

--- labs/lab4/confirmation.php
+++ labs/lab4/confirmation.php
@@ -1,6 +1,11 @@
 <?php
+session_start(); // Starts or resumes an existing session
+
+    $_SESSION['errors']  = array();
+    //$_COOKIES
+
     include('includes/data.php');
-    print_r($_GET);
+    //print_r($_GET); //FOR TESTING
     function getClientName(){
         return ucwords($_GET[fullName]); //capitalizes first letter of every word
     }
@@ -8,52 +13,87 @@
         return $_GET['candidate'];
     }
     function getProducts(){
-        $product_list= $_GET['poductToBuy'];
+        $productsToBuy= $_GET['productsToBuy'];
         $productList = getProductList();
         $total =0;
         
-        foreach($productList as $product){
+        echo "<br />";
+        foreach($productsToBuy as $product){
             echo $product . " " ."$".$productList[$product];
             $total += $productList[$product];
-            echo $product . "<br />";
+            echo $product . "<br /><br />";
         }
-        $magazine = $_GET['magazine'];
         
-        echo $magazine. "-month Magazine Subscription ($" .($$magazine*10).")<br />";
-        $total+=$magazine*10;
-        echo "Total: $" .$total;
+        $magazine = $_GET['magazine'];
+        echo $magazine. "-month Magazine Subscription ($" .($magazine*10).")<br />";
+        $total += $magazine*10;
+        echo "<br /><br />Total: $" .$total;
         
     }
 
     function isDataValid(){
         $dataValid = true;
-        if(empty(getCandidateName())){
-            echo "ERROR: Select a candidate.";
-            $dataValid = false;
-            header('Location: index.php'); // Sends user back to index.php
+        if(empty(getClientName())){
+            array_push($_SESSION['errors'], "ERROR: Enter your name.");
         }
-        if()
+        if(empty(getCandidateName())){
+            // echo "ERROR: Select a candidate.";
+            array_push($_SESSION['errors'],"ERROR: Select a candidate.");
+            $dataValid = false;
+            // header('Location: index.php'?error=YOU MUST SELECT A CANDIDATE); // Sends user back to index.php
+        }
+        // Validate age is not empty
+        if(empty($_GET['age'])){
+            array_push($_SESSION['errors'], "ERROR: You must entere your age.");
+            $dataValid =  false;
+        }
+        
+        // Validate age is 18 or older
+        else if($_GET['age'] < 18){
+            array_push($_SESSION['errors'],"ERROR: Must be 18 or older.");
+            $dataValid = false;
+        }
+        
+        if(!isset($_GET['productsToBuy']) && !isset($_GET['magazine'])){
+            array_push($_SESSION['errors'], "ERROR: Select a magazine plan.");
+            $dataValid = false;
+        }
+        
+        return $dataValid;
     }
-    isDataValid();
+    if (!isDataValid()){
+        header('Location: index.php');
+    }
 ?>
 
 <!DOCTYPE html>
 <html>
     <head>
-        <title>
-            
-        </title>
+        <title>Form Received</title>
+        <link href="../../assignments/styles.css" rel="stylesheet"/>
+        <link href="css/styles.css" rel="stylesheet" />
     </head>
     <body>
-        <br /><br />
-        Dear <strong><?=getClientName()?>,</strong>
-        <br />
-        <p>
-            <br />Thank you for supporting candidate <strong><?=getCandidateName()?></strong>
+        <nav>
+            <a href="index.php"> Home</a>
+        </nav>
+        <hr />
+        <main>
+            Dear <strong><?=getClientName()?>,</strong>
             <br />
-            <br />You ordered these products:
-            <br /><tab><?=getProducts()?></tab>
-            
-        </p>
+            <p>
+                <br />Thank you for supporting candidate <strong><?=getCandidateName()?></strong>
+                <br />
+                <br />You ordered these products:
+                <br /><tab><?=getProducts()?></tab>
+            </p>
+            <hr />
+            <footer>
+                &copy; Ceja, 2016 <br />
+                Disclaimer: The content of this web page might not be accurate.
+               <br />
+                <img src="../../img/peace.png" height="80px" width="70px" alt="CSUMB logo" />
+            </footer>
+        </main>
     </body>
 </html>
--- labs/lab4/index.php
+++ labs/lab4/index.php
@@ -1,6 +1,33 @@
-<?php
+<!--
 
+Lab 4: HTML Forms & PHP Assignment
+"Support your Presidential Candidate" Form
+
+Rubric:
+
+1) The main page includes all Form elements (20pts)
+2) The "confirmation" page displays all data from the Form (20pts)
+3) The total cost is calculated properly (20pts)
+4) There is validation for age (18 or older), candidate (not empty), and products (not empty) (15pts)
+5) Errors are properly displayed on the main page, upon submission of the form (15pts)
+6) There is an external CSS file (10pts)
+
+-->
+
+
+
+<?php
+    session_start();
     include('includes/data.php');
+    
+    //print_r($errorList); // FOR TESTING
+    
+    function displayErrors(){
+       $errorList = $_SESSION['errors'];
+        foreach($errorList as $error){
+            echo $error . "<br />";
+        }
+    }
 
 ?>
 
@@ -8,45 +35,62 @@
 <html>
     <head>
         <title>Home</title>
+        <link href="../../assignments/styles.css" rel="stylesheet"/>
+        <link href="css/styles.css" rel="stylesheet" />
     </head>
     <body>
-        <h1>Support Your Presidential Candidate</h1>
-        <center><img src="img/candidates.jpg" alt="candidates" width="400px" height="250px" /></center>
-        <form action="confirmation.php">
-            Enter your name: <input type="text" name="fullName" placeholder="Enter your full name" size="50"><br />
-            
-            Enter your age: <input type="number" name="age"><strong> (Must be 18 or older)</strong> <br />
-            
-            Select your candidate: 
-            <select name="candidate">
-                <?php 
-                    $candidateList= getCandidateList();
-                    foreach($candidateList as $candidate){
-                        echo "<option value='$candidate'>$candidate</option>";
+        <nav>
+            <a href="index.php" id="currentPage"> Home</a>
+        </nav>
+        <main>
+            <h1>Support Your Presidential Candidate</h1>
+            <center><img src="img/candidates.jpg" alt="candidates" width="400px" height="250px" /></center>
+            <br />
+            <form action="confirmation.php">
+                Enter your name: <input type="text" name="fullName" placeholder="Enter your full name" size="50"><br />
+                
+                Enter your age: <input type="number" name="age"><strong> (Must be 18 or older)</strong> <br />
+                
+                Select your candidate: 
+                <select name="candidate">
+                    <option value="">Select One</option>
+                    <?php 
+                        $candidateList= getCandidateList();
+                        foreach($candidateList as $candidate){
+                            echo "<option value='$candidate'>$candidate</option>";
+                        }
+                    ?>
+                </select>
+                <br />
+                <strong>Merchandise</strong>
+                <br />
+                <?php
+                    $productList=getProductList();
+                    foreach($productList as $product=>$price){
+                        echo "<input type='checkbox' name='productsToBuy[]' id='$product' value='$product'><label for=$product'>$product($$price)</label><br />";
                     }
+                
                 ?>
-            </select>
-            <br />
-            <strong>Merchandise</strong>
-            <br />
-            <?php
-                $productList=getProductList();
-                foreach($productList as $product=>$price){
-                    echo "<input type='checkbox' name='productsToBuy[]' id='$product' value='$product'><label for=$product'>$product($$price)</label><br />";
-                }
-            
-            ?>
-            
-            <strong>Campaign Magazine ($10 per month)</strong>
-            <br />
-            <input type="radio" name="magazine" value="1" id="one_month"><label for="oneMonth"> 1 month</label>
-            <input type="radio" name="magazine" value="3" id="three_months"><label for="threeMonths"> 3 months</label>
-            <input type="radio" name="magazine" value="6" id="six_months"><label for="sixMonths"> 6 months</label>
-            <input type="radio" name="magazine" value="12" id="twelve_months"><label for="twelveMonths"> 12 months</label>
-            <br />
-            
-            <input type="image" src="img/buy-now.gif" width="150px" value="submit form"/>
-        </form>
+                
+                <strong>Campaign Magazine ($10 per month)</strong>
+                <br />
+                <input type="radio" name="magazine" value="1" id="one_month"><label for="oneMonth"> 1 month</label>
+                <input type="radio" name="magazine" value="3" id="three_months"><label for="threeMonths"> 3 months</label>
+                <input type="radio" name="magazine" value="6" id="six_months"><label for="sixMonths"> 6 months</label>
+                <input type="radio" name="magazine" value="12" id="twelve_months"><label for="twelveMonths"> 12 months</label>
+                <br />
+                
+                <input type="image" src="img/buy-now.gif" width="150px" value="submit form"/>
+            </form>
+            <?=displayErrors()?>
+            <hr />
+            <footer>
+                &copy; Ceja, 2016 <br />
+                Disclaimer: The content of this web page might not be accurate.
+               <br />
+                <img src="../../img/peace.png" height="80px" width="70px" alt="CSUMB logo" />
+            </footer>
+        </main>
         
     </body>
 </html>
