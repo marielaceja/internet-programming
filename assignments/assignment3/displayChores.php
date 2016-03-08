5e5abe9de86e962ed37c39f5e6f34047e1eb079a
Mariela Ceja <maceja@csumb.edu>
2016-03-08 20:44:11 +0000

    Initialized assignment 3.

---
+++ assignments/assignment3/displayChores.php
@@ -0,0 +1,21 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chores</title>
+        <link href="../styles.css" rel="stylesheet">
+        <link href="css/styles.css" rel="stylesheet">
+    </head>
+    <body>
+        <nav>
+            <a href="index.php" id="currentPage">Home</a>
+        </nav>
+        <main>
+            
+        </main>
+    </body>
+</html>
