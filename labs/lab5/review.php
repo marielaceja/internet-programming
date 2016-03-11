1678c0d4395bd5df237b82f6fc8b5cf5b554de03
Mariela Ceja <maceja@csumb.edu>
2016-03-11 01:06:38 +0000

    Started lab 5. (Learning about databases)

---
+++ labs/lab5/review.php
@@ -0,0 +1,28 @@
+<?php
+
+    // 3 Methods to connect to the database:
+        // Use mysql_connect (rarely used because or reliability)
+        // mysql_i (mysql improved -- if using sql, use this)
+        // PHP Data Objects (PDO -- if intending to migrate, use this! Very flexible)
+        
+    
+
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <nav>
+            
+        </nav>
+        <hr />
+        <main>
+            
+        </main>
+    </body>
+</html>
